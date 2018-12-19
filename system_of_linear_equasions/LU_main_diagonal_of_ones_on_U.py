94044ec27ef89f6061f4e2621647a73fb66a9418
Anna Korunska <akorunska@gmail.com>
2018-12-19 17:15:38 +0200

    LU factor

--- system_of_linear_equasions/LU_main_diagonal_of_ones_on_U.py
+++ system_of_linear_equasions/LU_main_diagonal_of_ones_on_U.py
@@ -1,7 +1,7 @@
 from matrix import Matrix
 
 
-def LU_main_diagonal_of_ones_on_U(matrix):
+def getLU(matrix):
 	L = Matrix(matrix.n)
 	U = Matrix(matrix.n, extended=True)
 	p = 0
@@ -10,30 +10,36 @@ def LU_main_diagonal_of_ones_on_U(matrix):
 
 		i = j
 		while i < matrix.n:
-			print("L: %i %i" % (i, j))
+			# print(i, j)
+			# print("l[%i][%i] = a[%i][%i] = %i" % (i, j, i, j, matrix.values[i][j]))
 			L.values[i][j] = matrix.values[i][j]
-			print("l[%i][%i] = A[%i][%i] = %i" % (i, j, i, j, matrix.values[i][j]))
 			k = 0
-			while k < j - 1:
+			while k < j:
 				L.values[i][j] -= U.values[k][j] * L.values[i][k]
+				# print("l[%i][%i] -= u[%i][%i] * l[%i][%i] =  - %i * %i" % (i, j, k, j, i, k, U.values[k][j], L.values[i][k]))
 				k += 1
 			i += 1
-		print("L: ", L)
+			# print("L: ", L)
 
 		i = p
 		j = i
 		while j < matrix.n + 1:
-			print("U: %i %i" % (i, j))
 			U.values[i][j] = matrix.values[i][j]
 			k = 0
-			while k < i - 1:
+			while k < i:
 				U.values[i][j] -= U.values[k][j] * L.values[i][k]
 				k += 1
-			if (L.values[i][i] is not 0):
-				U.values[i][j] /= L.values[i][i] # вся разница между факторизациями в этой строчке????
+			U.values[i][j] /= L.values[i][i] # вся разница между факторизациями в этой строчке????
 			j += 1
-		print("U: ", U)
+		# print("U: ", U)
 
 		p += 1
+	return (L, U)
+
+def LU_main_diagonal_of_ones_on_U(matrix):
+	(L, U) = getLU(matrix)
 	print("L: ", L)
-	print("U: ", U)
+	print("U: ", U) 
+
+	init = L.mult(U)
+	print("L * U = ", init)
--- system_of_linear_equasions/main.py
+++ system_of_linear_equasions/main.py
@@ -6,14 +6,15 @@ from LU_main_diagonal_of_ones_on_U import LU_main_diagonal_of_ones_on_U
 
 def gaussian_elimination_demo():
 	m = read_matrix("matrixes/2.txt")
+	print("Initial matrix: ", m)
 	res = gaussian_elimination(m, choose_by_column_mode=True)
 	print("result is: \n", res)
 
 
 def LU_main_diagonal_of_ones_on_U_demo():
-	m = read_matrix("matrixes/1.txt")
+	m = read_matrix("matrixes/3.txt")
+	# m = Matrix(3, [[1, 3, 5], [2 , 4, 6], [0, 1, 6]], [1, 1, 1], True)
 	print("Initial matrix: ", m)
-
 	res = LU_main_diagonal_of_ones_on_U(m)
 
 
