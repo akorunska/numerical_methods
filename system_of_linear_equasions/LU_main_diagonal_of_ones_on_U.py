34bbbc7e89418114edbc1636aeab64116bfa00f4
Anna Korunska <akorunska@gmail.com>
2018-12-19 15:00:42 +0200

    LU factorisation breaks matrix in valid L and U matrixes

--- system_of_linear_equasions/LU_main_diagonal_of_ones_on_U.py
+++ system_of_linear_equasions/LU_main_diagonal_of_ones_on_U.py
@@ -1,4 +1,39 @@
+from matrix import Matrix
 
 
 def LU_main_diagonal_of_ones_on_U(matrix):
-	L = 
+	L = Matrix(matrix.n)
+	U = Matrix(matrix.n, extended=True)
+	p = 0
+	while p < matrix.n:
+		j = p
+
+		i = j
+		while i < matrix.n:
+			print("L: %i %i" % (i, j))
+			L.values[i][j] = matrix.values[i][j]
+			print("l[%i][%i] = A[%i][%i] = %i" % (i, j, i, j, matrix.values[i][j]))
+			k = 0
+			while k < j - 1:
+				L.values[i][j] -= U.values[k][j] * L.values[i][k]
+				k += 1
+			i += 1
+		print("L: ", L)
+
+		i = p
+		j = i
+		while j < matrix.n + 1:
+			print("U: %i %i" % (i, j))
+			U.values[i][j] = matrix.values[i][j]
+			k = 0
+			while k < i - 1:
+				U.values[i][j] -= U.values[k][j] * L.values[i][k]
+				k += 1
+			if (L.values[i][i] is not 0):
+				U.values[i][j] /= L.values[i][i] # вся разница между факторизациями в этой строчке????
+			j += 1
+		print("U: ", U)
+
+		p += 1
+	print("L: ", L)
+	print("U: ", U)
--- system_of_linear_equasions/main.py
+++ system_of_linear_equasions/main.py
@@ -1,6 +1,7 @@
 from matrix import Matrix
 from read_matrix import read_matrix
 from gaussian_elimination import gaussian_elimination
+from LU_main_diagonal_of_ones_on_U import LU_main_diagonal_of_ones_on_U
 
 
 def gaussian_elimination_demo():
@@ -9,8 +10,19 @@ def gaussian_elimination_demo():
 	print("result is: \n", res)
 
 
-gaussian_elimination_demo()
+def LU_main_diagonal_of_ones_on_U_demo():
+	m = read_matrix("matrixes/1.txt")
+	print("Initial matrix: ", m)
+
+	res = LU_main_diagonal_of_ones_on_U(m)
 
 
 
-# print(Matrix(2, [[2, 3], [8, 1]], [2, 3], extended=False))
+# SLE 1
+
+
+# SLE 2
+# gaussian_elimination_demo()
+
+# SLE 3
+LU_main_diagonal_of_ones_on_U_demo()
