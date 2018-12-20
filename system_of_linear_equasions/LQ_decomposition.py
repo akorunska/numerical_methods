2e95fb79799cbd90201a7150158d486b624a0d7a
Anna Korunska <akorunska@gmail.com>
2018-12-20 23:40:55 +0200

    LQ decomposition: computing answers for the sle

--- system_of_linear_equasions/LQ_decomposition.py
+++ system_of_linear_equasions/LQ_decomposition.py
@@ -13,8 +13,7 @@ def build_householder_matrix(w):
 	I = Matrix.get_unit_matrix(K.n)
 	return I.minus(K)
 
-
-def LQ_decomposition(m):
+def build_LQ(m):
 	beta = [0 for x in range(m.n - 1)]
 	mu = [0 for x in range(m.n - 1)]
 	A = [0 for x in range(m.n)]
@@ -38,6 +37,33 @@ def LQ_decomposition(m):
 		H = build_householder_matrix(w)
 
 		A[i + 1] = prev.mult(H)
-		print(A[i + 1])
-	print("thats all folks")
+		if Q is 0:
+			Q = H
+		else:
+			Q = Q.mult(H)
+		print("A(%i):" % (i + 1), A[i + 1])
+		# print("Q: ", Q)
+	return A[i + 1], Q
+
+def get_answer(m, L, Q_transp):
+	y = [0 for x in range(m.n)]
+	y[0] = m.r[0] / L.values[0][0]
+	for i in range(1, m.n):
+		y[i] = m.r[i]
+		for k in range(i):
+			y[i] -= L.values[i][k] * y[k]
+		y[i] /= L.values[i][i]
+	y = [[val] for val in y]
+	y_matr = Matrix(n=1, m=3, values=y)
+	x = Q_transp.mult(y_matr)
+	x = x.transpose()
+	return x.values[0]
+
+
+def LQ_decomposition(m):
+	L, Q = build_LQ(m)
+	print("L: ", L, "Q: ", Q)
+	print("L * Q^T = ", L.mult(Q.transpose()))
+	return get_answer(m, L, Q)
+
 
--- system_of_linear_equasions/main.py
+++ system_of_linear_equasions/main.py
@@ -4,12 +4,19 @@ from gaussian_elimination import gaussian_elimination
 from LU_main_diagonal_of_ones_on_U import LU_main_diagonal_of_ones_on_U
 from LU_main_diagonal_of_ones_on_L import LU_main_diagonal_of_ones_on_L
 from tomas_algorithm import tomas_algorithm
+from LQ_decomposition import LQ_decomposition
 
 
 ## DEMOS FOR SLE 1	
 
 def LQ_decomposition_demo(): # метод відбиття на основі LQ факторизації
-	pass
+	m = read_matrix("matrixes/1.txt")
+	print("Initial matrix: ", m)
+	res = LQ_decomposition(m)
+	print("result is: \n", res)
+	# print("\ninverce matrix based on LU factorisation: ", inv_matrix.get_precise_to_str())
+	# print("A * A^(-1) = ", m.mult(inv_matrix))
+	# print("det(A) = ", det)
 
 def LU_main_diagonal_of_ones_on_L_demo():
 	m = read_matrix("matrixes/1.txt")
@@ -58,6 +65,7 @@ def LU_main_diagonal_of_ones_on_U_demo():
 
 # SLE 1
 # LU_main_diagonal_of_ones_on_L_demo()
+LQ_decomposition_demo()
 
 
 # SLE 2
