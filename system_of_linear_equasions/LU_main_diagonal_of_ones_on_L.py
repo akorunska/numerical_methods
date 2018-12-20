592ed0da72e479a4137e3ea38efbd9545c67d717
Anna Korunska <akorunska@gmail.com>
2018-12-20 13:54:42 +0200

    LU factorisation with diagonal of ones on L works

---
+++ system_of_linear_equasions/LU_main_diagonal_of_ones_on_L.py
@@ -0,0 +1,99 @@
+from matrix import Matrix
+
+
+def getLU(matrix):
+	L = Matrix(matrix.n)
+	U = Matrix(matrix.n, extended=True)
+	p = 0
+	while p < matrix.n:
+		j = p
+		i = j
+
+		while j < matrix.n:
+			U.values[i][j] = matrix.values[i][j]
+			k = 0
+			while k < i:
+				U.values[i][j] -= U.values[k][j] * L.values[i][k]
+				k += 1
+			j += 1
+
+		i = p
+		j = i
+		while i < matrix.n:
+			L.values[i][j] = matrix.values[i][j]
+			k = 0
+			while k < j:
+				L.values[i][j] -= U.values[k][j] * L.values[i][k]
+				k += 1
+			L.values[i][j] /= U.values[j][j]
+			i += 1
+
+		p += 1
+	return (L, U)
+
+
+# def get_result(L, U):
+# 	n = L.n
+# 	x = [0 for x in range(n)]
+# 	i = n - 1
+# 	while i >= 0:
+# 		x[i] = U.values[i][n]
+# 		k = i + 1
+# 		while k < n:
+# 			x[i] -= U.values[i][k] * x[k]
+# 			k += 1
+# 		i -= 1
+# 	return x
+
+
+# def cron(i, j):
+# 	if i == j:
+# 		return 1
+# 	return 0
+
+
+# def get_inverce_matrix(L, U):
+# 	X = Matrix(L.n)
+# 	p = L.n - 1
+# 	while p >= 0:
+# 		i = p
+# 		j = i
+# 		while j >= 0:
+# 			X.values[i][j] = cron(i, j)
+# 			k = j + 1
+# 			while k < L.n:
+# 				X.values[i][j] -= L.values[k][j] * X.values[i][k]
+# 				k += 1
+# 			X.values[i][j] /= L.values[j][j]
+# 			j -= 1
+# 		j = p
+# 		i = j - 1
+# 		while i >= 0:
+# 			k = i + 1
+# 			while k < L.n:
+# 				X.values[i][j] -= U.values[i][k] * X.values[k][j]
+# 				k += 1
+# 			i -= 1
+# 		p -= 1
+# 	return X
+
+
+# def get_determinant(L):
+# 	det = 1;
+# 	for i in range(L.n):
+# 		det *= L.values[i][i]
+# 	return det
+
+
+def LU_main_diagonal_of_ones_on_L(matrix):
+	(L, U) = getLU(matrix)
+	print("L: ", L)
+	print("U: ", U) 
+
+	init = L.mult(U)
+	print("L * U = ", init)
+
+	# ans = get_result(L, U)
+	# inv_matrix = get_inverce_matrix(L, U)
+	# det = get_determinant(L)
+	# return ans, inv_matrix, det
--- system_of_linear_equasions/main.py
+++ system_of_linear_equasions/main.py
@@ -2,7 +2,17 @@ from matrix import Matrix
 from read_matrix import read_matrix
 from gaussian_elimination import gaussian_elimination
 from LU_main_diagonal_of_ones_on_U import LU_main_diagonal_of_ones_on_U
+from LU_main_diagonal_of_ones_on_L import LU_main_diagonal_of_ones_on_L
 
+
+def LU_main_diagonal_of_ones_on_L_demo():
+	m = read_matrix("matrixes/0.txt")
+	print("Initial matrix: ", m)
+	LU_main_diagonal_of_ones_on_L(m)
+	# print("result is: \n", res)
+	# print("\ninverce matrix based on LU factorisation: ", inv_matrix.get_precise_to_str())
+	# print("A * A^(-1) = ", m.mult(inv_matrix))
+	# print("det(A) = ", det)
 
 def gaussian_elimination_demo():
 	m = read_matrix("matrixes/2.txt")
@@ -29,4 +39,4 @@ def LU_main_diagonal_of_ones_on_U_demo():
 # gaussian_elimination_demo()
 
 # SLE 3
-LU_main_diagonal_of_ones_on_U_demo()
+LU_main_diagonal_of_ones_on_L_demo()
