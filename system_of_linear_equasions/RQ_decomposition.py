98ea55a7dd03f7c6bec01257eb2b92e4d4f64a0d
Anna Korunska <akorunska@gmail.com>
2018-12-21 22:25:14 +0200

    additional RQ decomposition

--- system_of_linear_equasions/RQ_decomposition.py
+++ system_of_linear_equasions/RQ_decomposition.py
@@ -13,7 +13,7 @@ def build_householder_matrix(w):
 	I = Matrix.get_unit_matrix(K.n)
 	return I.minus(K)
 
-def build_QL(m):
+def build_RQ(m):
 	beta = [0 for x in range(m.n - 1)]
 	mu = [0 for x in range(m.n - 1)]
 	A = [0 for x in range(m.n)]
@@ -26,7 +26,7 @@ def build_QL(m):
 
 		row_sq_sum = 0
 		for k in range(n - i + 1):
-			row_sq_sum += prev.values[k][n - i] ** 2
+			row_sq_sum += prev.values[n - i][k] ** 2
 		beta[i] = sign(-prev.values[n - i][n - i]) * math.sqrt(row_sq_sum)
 
 		mu[i] = 1 / (math.sqrt((2 * beta[i] ** 2) - 2 * beta[i] * prev.values[n - i][n - i]))
@@ -34,20 +34,20 @@ def build_QL(m):
 		w_values = [[0] for k in range(m.n)]
 
 		for k in range(n - i + 1):
-			w_values[k] = [mu[i] * prev.values[k][n - i]]
+			w_values[k] = [mu[i] * prev.values[n - i][k]]
 			if k is n - i:
-				w_values[k] = [mu[i] * (prev.values[k][n - i] - beta[i])]
+				w_values[k] = [mu[i] * (prev.values[n - i][k] - beta[i])]
 
 		w = Matrix(n=1, m=m.n, values=w_values)
 		print(w_values)
 		H = build_householder_matrix(w)
-		print (H.get_precise_to_str())
+		print ("H(i): " % i, H.get_precise_to_str())
 
-		A[i + 1] = H.mult(A[i])
+		A[i + 1] = A[i].mult(H)
 		if Q is 0:
 			Q = H
 		else:
-			Q = H.mult(Q)
+			Q = Q.mult(H)
 		print("A(%i):" % (i + 1), A[i + 1].get_precise_to_str())
 		# print("Q: ", Q)
 	return A[i + 1], Q
@@ -67,14 +67,8 @@ def get_answer(m, L, Q_transp):
 	return x
 	
 
-# def get_determinant(L):
-# 	det = 1;
-# 	for i in range(L.n):
-# 		det *= L.values[i][i]
-# 	return det
-
-def QL_decomposition(m):
-	L, Q = build_QL(m)
-	print("L: ", L, "Q: ", Q)
-	print("Q^T * L = ", Q.transpose().mult(L))
-	return get_answer(m, L, Q)
+def RQ_decomposition(m):
+	R, Q = build_RQ(m)
+	print("R: ", R, "Q: ", Q)
+	print("Q^T * R = ", R.mult(Q.transpose()))
+	return get_answer(m, R, Q)
--- system_of_linear_equasions/kr.py
+++ system_of_linear_equasions/kr.py
@@ -1,5 +1,5 @@
 from read_matrix import read_matrix
-from QL_decomposition import QL_decomposition
+from RQ_decomposition import RQ_decomposition
 from gaussian_elimination import gaussian_elimination
 
 
@@ -8,5 +8,5 @@ print("Initial matrix: ", m)
 
 # QL_decomposition(m)
 
-res = QL_decomposition(m)
+res = RQ_decomposition(m)
 print("result is: \n", res)
