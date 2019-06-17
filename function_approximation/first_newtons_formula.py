1ca2c6d20504106243392bae99aece0eced85fed
Anna Korunska <akorunska@gmail.com>
2019-06-17 22:43:43 +0300

    refactoring approximation

--- function_approximation/first_newtons_formula.py
+++ function_approximation/first_newtons_formula.py
@@ -20,19 +20,19 @@ x_values=np.linspace(6, 20, len(control_points))
 y_values = np.array(list(map(f, x_values)))
 
 
-def Newton(x, x_values, y_values):
-    n=len(x_values)
-    f=np.zeros((n,n))
-    f[:,0]=y_values
-    for i in range(n-1,0,-1):
+def first_newtons_formula(x, x_values, y_values):
+    n = len(x_values)
+    f = np.zeros((n, n))
+    f[:, 0] = y_values
+    for i in range(n - 1, 0, -1):
+        for j in range(0, i):
+            f[n - i + j, n-i] = (f[n - i + j, n - i - 1] - f[n - i + j - 1, n - i - 1]) / (x_values[n - i + j] - x_values[j])
+    P = 0
+    for i in range(0, n):
+        a = f[i, i]
         for j in range(0,i):
-            f[n-i+j,n-i]=(f[n-i+j,n-i-1]-f[n-i+j-1,n-i-1])/(x_values[n-i+j]-x_values[j])
-    P=0
-    for i in range(0,n):
-        a=f[i,i]
-        for j in range(0,i):
-            a=a*(x-x_values[j])
-        P=P+a
+            a = a * (x - x_values[j])
+        P = P + a
     return P
 
 if __name__=="__main__":
@@ -40,7 +40,7 @@ if __name__=="__main__":
     print(y_values)
 
     x=Symbol('X')
-    P=Newton(x,x_values,y_values)
+    P=first_newtons_formula(x, x_values, y_values)
     print('The interpolating polynomial is:', expand(P))
 
     x_approx = np.array(control_points)
